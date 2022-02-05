c41a2c91c2ae1fb67a253fc7fc90fed6abb40edb
Eoin Bathurst <silentechouk@gmail.com>
2022-02-05 19:22:36 +0100

    switch to a root character data store

--- src/lib/stores/characterData.ts
+++ src/lib/stores/characterData.ts
@@ -1,9 +1,3 @@
-import { indexedDBStorage, persist } from "@macfja/svelte-persistent-store";
-import { writable } from "svelte/store";
-import baseCharacterData from "$data/character";
+import getBaseCharacterStore from "$data/character";
 
-export const characterData = persist(
-  writable(baseCharacterData()),
-  indexedDBStorage(),
-  "characterData"
-);
+export const rootCharacterData = getBaseCharacterStore();
