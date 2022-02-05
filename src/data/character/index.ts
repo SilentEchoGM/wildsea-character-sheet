185e5432f4c239144ab2016e2baf2ccf05940211
Eoin Bathurst <silentechouk@gmail.com>
2022-02-05 19:19:28 +0100

    start using immer

--- src/data/character/index.ts
+++ src/data/character/index.ts
@@ -5,48 +5,57 @@ import { pipe } from "fp-ts/function";
 import { array as A } from "fp-ts";
 import type { CharacterData, Resource } from "$types/data";
 import { backgroundProperties } from "./backgroundProperties";
+import { writable } from "svelte/store";
+import type { Writable } from "svelte/store";
 
-export default (): CharacterData => ({
-  background: pipe(
-    [...backgroundProperties],
-    A.mapWithIndex((order, label) => ({
-      label,
-      order,
-      text: "",
-    }))
-  ),
-  backgroundNotes: [],
-  milestone: [],
-  drives: [],
-  mires: [],
-  notes: [],
-  edge: pipe(
-    [...edgeList],
-    A.map((label) => ({
-      label,
-      max: 1,
-      value: 0,
-    }))
-  ),
-  languages: pipe(
-    [...languageList],
-    A.map((label) => ({
-      label,
-      max: 1,
-      value: 0,
-    }))
-  ),
-  skills: pipe(
-    [...skillList],
-    A.map((skill) => {
-      return {
-        ...skill,
-        max: 3,
+export default (): Writable<CharacterData> =>
+  writable({
+    fixedBackgroundFields: pipe(
+      [...backgroundProperties],
+      A.mapWithIndex((order, label) => ({
+        label,
+        order,
+        text: "",
+      }))
+    ),
+    customBackgroundFields: [
+      {
+        label: "",
+        order: 1,
+        text: "",
+      },
+    ],
+    milestone: [],
+    drives: [],
+    mires: [],
+    notes: [],
+    edge: pipe(
+      [...edgeList],
+      A.map((label) => ({
+        label,
+        max: 1,
         value: 0,
-      };
-    })
-  ),
-  aspects: [],
-  tracks: [],
-  resources: [] as Resource[],
-});
+      }))
+    ),
+    languages: pipe(
+      [...languageList],
+      A.map((label) => ({
+        label,
+        max: 1,
+        value: 0,
+      }))
+    ),
+    skills: pipe(
+      [...skillList],
+      A.map((skill) => {
+        return {
+          ...skill,
+          max: 3,
+          value: 0,
+        };
+      })
+    ),
+    aspects: [],
+    tracks: [],
+    resources: [] as Resource[],
+  });
